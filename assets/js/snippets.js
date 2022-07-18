dc906822dfeed46a166159bd55618507c4e3df94
Arjen de Aldrey <adealdrey3@gmail.com>
2022-07-18 09:03:57 -0400

    [ADD] Landing page added, hero section and nav bar done

---
+++ assets/js/snippets.js
@@ -0,0 +1,5 @@
+function copyDiscordToClipboard() {
+    /* global navigator, alert */
+    navigator.clipboard.writeText("Theodorus#3166");
+    alert("Saved the discord username Theodorus#3166 to the clipboard.");
+}
