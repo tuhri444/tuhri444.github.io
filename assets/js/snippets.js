6416420aaf1ad753a2410c91f812f52ea74625b1
Arjen de Aldrey <adealdrey1@gmail.com>
2022-08-10 14:48:55 +0200

    [ADD] Finished contact section

--- assets/js/snippets.js
+++ assets/js/snippets.js
@@ -3,3 +3,13 @@ function copyDiscordToClipboard() {
     navigator.clipboard.writeText("Theodorus#3166");
     alert("Saved the discord username Theodorus#3166 to the clipboard.");
 }
+
+function copyEmailToClipboard() {
+    /* global navigator, alert */
+    navigator.clipboard.writeText("adealdrey1@gmail.com");
+    alert("Saved the email adealdrey1@gmail.com to the clipboard.");
+}
+
+function openLinkedIn(link) {
+    window.open(link, '_blank').focus();
+}
