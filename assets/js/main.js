3d1f6bb0904aebcb7fbfcec01d61591326e37a57
Arjen de Aldrey <adealdrey3@gmail.com>
2022-08-18 15:45:38 +0200

    [ADD] Pacman project

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,7 +222,7 @@
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: true
     },
     pagination: {
       el: '.swiper-pagination',
