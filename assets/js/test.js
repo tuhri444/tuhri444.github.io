2f89ea8e7279e4e33c464b901c864b9e166965df
Arjen de Aldrey <57641923+tuhri444@users.noreply.github.com>
2023-08-10 10:30:27 +0200

    Create test.js

---
+++ assets/js/test.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  RedirectWithLink();
+}
+
+const RedirectWithLink = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    var redirection = urlParams.get('redirect');
+    
+    if (redirection == null){
+        var linkToAR = "adsquares.nl/AR-luigi?redirect=";
+        const path = window.location.host;
+        const index = path.indexOf('/');
+        var pathAddition = path.substring(index + 1);
+        pathAddition = pathAddition.slice(0, -1);
+        if (index !== -1) {
+            linkToAR += pathAddition;
+            
+            //Check if board game link
+            if (pathAddition.includes("qrcodespel")){
+                linkToAR += "&boardgame=true";
+            }
+            
+            window.location.href = linkToAR;
+        }
+        console.error('Couldn\'t get path from URL');
+    }
+    else{
+        window.location.href = window.location.search;
+        return;
+    }
+}
